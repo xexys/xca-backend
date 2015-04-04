93c88590e9367ab90d7d441b670c79fb386cc997
xexys <xexys@mail.ru>
2015-04-04 16:47:29 +0300

    Удаляем старые папки

--- backend/config/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return CMap::mergeArray(
-    require Yii::getPathOfAlias('common.config.main') . '.php',
-    require __DIR__ . '/overrides/base.php',
-    require __DIR__ . '/overrides/' . (PROD_MODE ? 'prod' : 'dev') . '.php'
-);
