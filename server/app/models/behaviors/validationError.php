b9f8f67f36d7cbb45140aa9202c7f01048ce3e0b
xexys <xexys@mail.ru>
2015-08-08 16:30:00 +0300

    Разработка

---
+++ server/app/models/behaviors/validationError.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 08.08.15
+ * Time: 17:09
+ */
+
+namespace app\models\behaviors;
+
+
+class ValidationError extends \CBehavior
+{
+    public function getFirstErrorMessage()
+    {
+        $errors = $this->getOwner()->getErrors();
+        if ($errors) {
+            return reset($errors)[0];
+        }
+    }
+}
